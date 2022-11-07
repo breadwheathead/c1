640434fa70cb1fbfbe733baabb3cc6c4045f4786
Илья <breadwheatbox@gmail.com>
2022-11-07 19:24:49 +0700

    task_5

--- task_5.py
+++ task_5.py
@@ -12,7 +12,16 @@ result = [23, 1, 3, 10, 4, 11]
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
-print([item for item in src if src.count(item) == 1])
+print([item for item in src if src.count(item) == 1])  # O(n^2)
 
+uniq_nums = set()
+tmp_nums = set()
 
+for item in src:
+    if item not in tmp_nums:
+        uniq_nums.add(item)
+    else:
+        uniq_nums.discard(item)
+    tmp_nums.add(item)
 
+print([num for num in src if num in uniq_nums])
